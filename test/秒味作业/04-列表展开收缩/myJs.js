53a47fe18baed3ec2ee5790e318df9f524ba8d96
xuxusheng <xs20691718@163.com>
2016-03-12 23:26:09 +0800

    列表展开完善

--- test/秒味作业/04-列表展开收缩/myJs.js
+++ test/秒味作业/04-列表展开收缩/myJs.js
@@ -3,5 +3,26 @@ var level_2;
 
 
 window.onload = function(){
+	var aP = document.getElementsByClassName("ap")
+	for (var i=0; i<aP.length; i++){
+		aP[i].addEventListener('click',function(){
+			var nextUl = this.nextSibling;
+			var now = getStyle(nextUl,display);
+			if(now == 'none'){
+				nextUl.style.display = 'none';
+			}else{
+				nextUl.style.display = 'block';
+			}
+		})
+	}
+}
 
+
+
+function getStyle(obj,attr,value){
+	if(obj.currentStyle){
+		return obj.currentStyle[attr];
+	}else{
+		return obj.getComputedStyle(obj,false)[attr];
+	}
 }
